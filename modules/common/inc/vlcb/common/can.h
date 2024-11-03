63270ed6c5e5238924ee1b28e9cdd1d3bcc339bd
Freja <freja@freja.codes>
2024-11-03 13:40:31 +0100

    refactor enum to ensure it's datatype size which is useful for serialization

--- modules/common/inc/vlcb/common/can.h
+++ modules/common/inc/vlcb/common/can.h
@@ -2,6 +2,7 @@
 
 #include <inttypes.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "vlcb_defs.h"
 
@@ -10,18 +11,18 @@ typedef uint8_t CanId;
 int vlcb_defs_NewCanId(uint8_t value, CanId *const id);
 bool vlcb_defs_IsCanIdValid(CanId id);
 
-typedef enum {
+typedef uint8_t VlcbCanPriority;
+enum VlcbCanPriority {
   VLCB_CAN_PRIO_SELF_ENUM = 0x0,
   VLCB_CAN_PRIO_HIGH = 0x4,
   VLCB_CAN_PRIO_ABOVE = 0x5,
   VLCB_CAN_PRIO_NORMAL = 0x6,
   VLCB_CAN_PRIO_LOW = 0x7,
   VLCB_CAN_PRIO_LOWEST = 0xF,
-} VlcbCanPriority;
+};
 
 #define VLCB_CAN_PRIO_DEFAULT VLCB_CAN_PRIO_NORMAL
 
 bool vlcb_defs_IsCanPriorityValid(VlcbCanPriority prio);
 VlcbCanPriority vlcb_defs_CanPriorityFromOpcode(VlcbOpCode opc);
-int vlcb_defs_CanPriorityFromByte(
-    uint8_t val, VlcbCanPriority *const prio);
+int vlcb_defs_CanPriorityFromByte(uint8_t val, VlcbCanPriority *const prio);
