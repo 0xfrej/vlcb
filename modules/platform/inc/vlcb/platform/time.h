390e9e1208e2ee71c327abb8392103bbb3509e5c
Freja <freja@freja.codes>
2025-04-17 15:10:43 +0200

    clean up time interface

--- modules/platform/inc/vlcb/platform/time.h
+++ modules/platform/inc/vlcb/platform/time.h
@@ -2,21 +2,23 @@
 
 #include <stdint.h>
 
-typedef uint32_t vlcb_clock;
+#define VLCB_TIME_CLOCK_MAX UINT16_MAX
 
-// TODO: implement resolvers, diff, constants or other mechanisms? to resolve
-// ns/ms/s from clock
-//  implement an overflow count that can be used in-library to accurately detect
-//  time spans (only if needed honestly - depends on how fast the overflow
-//  happens)
-vlcb_clock vlcb_time_Milis();
+/**
+ * Vlcb time representation in millisecond resolution
+ *
+ * This abstraction is crucial as different platforms
+ * use different (or none) implementations of `time.h`
+ * and are therefore unreliable.
+ */
+typedef uint16_t vlcb_clock;
 
-#define VLCB_MILIS_MAX UINT32_MAX
+vlcb_clock vlcb_time_GetClock();
 
-static inline vlcb_clock vlcb_time_DiffMilis(const vlcb_clock start,
+static inline vlcb_clock vlcb_time_ClockDiff(const vlcb_clock start,
                                              const vlcb_clock end) {
   if (end < start) {
-    return (VLCB_MILIS_MAX - start) + end;
+    return (VLCB_TIME_CLOCK_MAX - start) + end;
   }
   return (end - start);
 }
--- modules/platform/src/arch/generic/time.c
+++ modules/platform/src/arch/generic/time.c
@@ -1,11 +1,7 @@
 #include "vlcb/platform/time.h"
 
-#include <stdint.h>
-#include "vlcb/platform/time.h"
+#include <time.h>
 
-// TODO: refactor
-vlcb_clock vlcb_time_Milis() {
-  struct timespec ts;
-  clock_gettime(CLOCK_REALTIME, &ts);
-  return ts.tv_sec * 1000U + ts.tv_nsec / 1000000;
+vlcb_clock vlcb_time_GetClock() {
+  return (vlcb_clock)((clock() * 1000) / CLOCKS_PER_SEC);
 }
